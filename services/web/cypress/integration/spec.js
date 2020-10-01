ae9da58f9b57c3d082e5f8b5dc8a12efb611129b
barnadsv <barnadsv@gmail.com>
2020-10-01 03:21:03 +0000

    Usando testing-library com cypress

--- services/web/cypress/integration/spec.js
+++ services/web/cypress/integration/spec.js
@@ -1,5 +1,15 @@
 import posts from "../../src/routes/blog/_posts";
 
+describe("Sapper template app", () => {
+  beforeEach(() => {
+    cy.visit("/");
+  });
+  it("has the correct heading", () => {
+    // https://testing-library.com/docs/guide-which-query
+    cy.findByRole("heading").should("contain", "Great success!");
+  });
+});
+
 describe("Blog posts", () => {
   beforeEach(() => {
     cy.visit("/blog");
