acc2f0fabdd4bad3f0cc207d88d0006227ddc001
barnadsv <barnadsv@gmail.com>
2020-10-01 02:59:12 +0000

    Cypress com caso de teste dinamico

--- services/web/cypress/integration/spec.js
+++ services/web/cypress/integration/spec.js
@@ -1,8 +1,19 @@
+import posts from "../../src/routes/blog/_posts";
+
 describe("Blog posts", () => {
   beforeEach(() => {
     cy.visit("/blog");
   });
   it("has the correct <h1>", () => {
     cy.contains("h1", "Recent posts");
+  });
+  posts.forEach((post) => {
+    it(`lists the "${post.title}" blog post`, () => {
+      cy.contains("[data-cy=blog-posts-list] li a", post.title).should(
+        "have.attr",
+        "href",
+        `blog/${post.slug}`
+      );
+    });
   });
 });
