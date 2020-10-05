6b48cf1ae7c17dd1701b522ed08863a7714078f5
barnadsv <barnadsv@gmail.com>
2020-10-05 20:03:48 +0000

    Verificando se os testes e o deploy para service/web funcionam com o cache

--- services/web/src/routes/blog/index.svelte
+++ services/web/src/routes/blog/index.svelte
@@ -1,9 +1,6 @@
 <script context="module">
 	export function preload({ params, query }) {
-		// return this.fetch(`blog.json`).then(r => r.json()).then(posts => {
-		// 	return { posts };
-        // });
-        if (typeof window !== "undefined") { 
+		if (typeof window !== "undefined") { 
             return window.db
             .collection("posts")
             .get()
@@ -41,10 +38,6 @@
 
 <ul data-cy="blog-posts-list">
 	{#each posts as post}
-		<!-- we're using the non-standard `rel=prefetch` attribute to
-				tell Sapper to load the data for the page as soon as
-				the user hovers over the link or taps it, instead of
-				waiting for the 'click' event -->
 		<li><a rel='prefetch' href='blog/{post.slug}'>{post.title}</a></li>
 	{/each}
 </ul>
