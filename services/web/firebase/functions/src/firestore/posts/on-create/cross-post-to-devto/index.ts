98f5faf9b1ffabc8eb73435365f9a8f6ad9c52fa
barnadsv <barnadsv@gmail.com>
2020-10-05 15:08:54 +0000

    Verificando se o workflow de functions funciona

--- services/web/firebase/functions/src/firestore/posts/on-create/cross-post-to-devto/index.ts
+++ services/web/firebase/functions/src/firestore/posts/on-create/cross-post-to-devto/index.ts
@@ -1,12 +1,3 @@
-// import * as functions from 'firebase-functions';
-
-// export default functions.firestore
-// .document('posts/{slug}')
-// .onCreate((snapshot: functions.firestore.DocumentSnapshot, context: functions.EventContext) => {
-//     console.log(`New post created with slug: ${context.params.slug}. Post: ${JSON.stringify(snapshot.data())}`);
-//     return null;
-// });
-
 import * as functions from "firebase-functions";
 import got from "got";
 
